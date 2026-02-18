027ca0f2364d8d109e59df0a18af8eaeb1a92072
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-02-18 23:31:52 +0600

    first commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const scoreDisplay = document.getElementById('score');
 function randomHole() {
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
-    if (hole === lastHole) return randomHole(); // Don't repeat holes
+    if (hole === lastHole) return randomHole(); 
     lastHole = hole;
     return hole;
 }
@@ -18,7 +18,7 @@ function showBug() {
     const hole = randomHole();
     hole.classList.add('active');
     
-    // Bug stays for a random time between 600ms and 1s
+   
     const time = Math.random() * (1000 - 600) + 600;
     
     setTimeout(() => {
@@ -27,13 +27,12 @@ function showBug() {
     }, time);
 }
 
-// Click listener for smashing bugs
 holes.forEach(hole => {
     hole.addEventListener('click', () => {
         if (hole.classList.contains('active')) {
             score++;
             scoreDisplay.textContent = score;
-            hole.classList.remove('active'); // Bug smashed!
+            hole.classList.remove('active'); 
         }
     });
 });
@@ -43,5 +42,5 @@ document.getElementById('start-btn').addEventListener('click', () => {
     scoreDisplay.textContent = 0;
     isGameOver = false;
     showBug();
-    setTimeout(() => isGameOver = true, 15000); // 15-second game
+    setTimeout(() => isGameOver = true, 15000); 
 });
